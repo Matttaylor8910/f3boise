7c31f055cdfb6fe426d1688bd1a89d703e310d60
Matt Taylor <8845360+Matttaylor8910@users.noreply.github.com>
2023-03-06 10:31:04 -0700

    Add filtering to the Q Line Up page

--- src/app/pages/q-line-up/q-line-up.page.ts
+++ src/app/pages/q-line-up/q-line-up.page.ts
@@ -7,12 +7,23 @@ import {QLineUp} from 'types';
 interface DateRow {
   date: string;
   displayDate: string;
-  cols: (QCell|undefined)[];
+  cols: QCell[];
+  qs: Set<string>;
+  hidden: boolean;
 }
 
 interface QCell extends QLineUp {
   warning: boolean;
   taken: boolean;
+  hidden: boolean;
+  hideText: boolean;
+  transparent: boolean;
+}
+
+interface AoColumn {
+  name: string;
+  qs: Set<string>;
+  hidden: boolean;
 }
 
 const FORMAT = 'YYYY-MM-DD';
@@ -26,7 +37,10 @@ export class QLineUpPage {
   days = 50;  // days to load at a time
   warningDays = 2;
 
-  aos: string[] = [];
+  filterText = '';
+  everythingHidden = false;
+
+  aos: AoColumn[] = [];
   dates: DateRow[] = [];
 
   constructor(
@@ -40,7 +54,10 @@ export class QLineUpPage {
     // a map whose key is the date, the value is another map where the key is
     // the ao and the value the text to display
     const dateMap = new Map<string, Map<string, QCell>>();
-    const aos = new Set<string>();
+    const aos = new Map<string, AoColumn>();
+    if (this.aos) {
+      this.aos.forEach(ao => aos.set(ao.name, ao));
+    }
 
     const to = moment(from).add(this.days, 'days').format(FORMAT);
     const qs = await this.qService.getQLineUp(from, to);
@@ -49,25 +66,37 @@ export class QLineUpPage {
       const days = moment(q.date).diff(moment().startOf('day'), 'days');
       const taken = q.qs !== null && q.qs.length > 0;
       const warning = days <= this.warningDays && !taken;
-      if (warning) {
-        console.log(days, q.date, moment().startOf('day').format(FORMAT));
-      }
       const aoMap = dateMap.get(q.date) ?? new Map<string, QCell>();
-      aoMap.set(q.ao, {...q, warning, taken});
+      aoMap.set(q.ao, {
+        ...q,
+        warning,
+        taken,
+        hidden: false,
+        transparent: false,
+        hideText: false,
+      });
       dateMap.set(q.date, aoMap);
-      aos.add(q.ao);
+
+      const ao = aos.get(q.ao) ?? {
+        name: q.ao,
+        qs: new Set<string>(),
+        hidden: false,
+      };
+      q.qs?.forEach(name => ao.qs.add(name));
+      aos.set(q.ao, ao);
     });
 
-    this.aos = Array.from(aos).sort((a, b) => a.localeCompare(b));
+    this.aos =
+        Array.from(aos.values()).sort((a, b) => a.name.localeCompare(b.name));
 
     let lastDate: string;
     Array.from(dateMap).map(item => {
       const [date, aoMap] = item;
 
       // build up the cols for this date
-      const cols: (QCell|undefined)[] = [];
+      const cols: QCell[] = [];
       for (let i = 0; i < this.aos.length; i++) {
-        cols.push(aoMap.get(this.aos[i]));
+        cols.push(this.getCell(date, this.aos[i].name, aoMap));
       }
 
       // add in empty days if they're missing
@@ -77,7 +106,9 @@ export class QLineUpPage {
           const diff = moment(date).diff(lastMoment, 'day');
           lastDate = lastMoment.format(FORMAT);
           if (diff > 0) {
-            this.addDate(lastDate, new Array(this.aos.length));
+            this.addDate(lastDate, this.aos.map(ao => {
+              return this.getCell(lastDate, ao.name);
+            }));
           }
         } else {
           lastDate = date;
@@ -87,6 +118,8 @@ export class QLineUpPage {
       // finally push this date
       this.addDate(date, cols);
     });
+
+    this.applyFilter();
   }
 
   loadMore() {
@@ -94,11 +127,65 @@ export class QLineUpPage {
     this.loadQLineup(moment(date).add(1, 'day').format(FORMAT));
   }
 
-  addDate(date: string, cols: (QCell|undefined)[]) {
+  addDate(date: string, cols: QCell[]) {
     let displayDate = moment(date).format('ddd, M/D');
     if (date === moment().format(FORMAT)) {
       displayDate = `Today, ${moment(date).format('M/D')}`
     }
-    this.dates.push({date, displayDate, cols});
+
+    const qs = new Set<string>();
+    cols.forEach(col => {
+      col.qs?.forEach(q => {
+        if (q) qs.add(q);
+      });
+    });
+
+    this.dates.push({date, displayDate, cols, qs, hidden: false});
+  }
+
+  applyFilter() {
+    for (const ao of this.aos) {
+      ao.hidden = this.shouldBeHidden(ao.qs, this.filterText);
+    }
+    for (const date of this.dates) {
+      date.hidden = this.shouldBeHidden(date.qs, this.filterText);
+
+      for (const cell of date.cols) {
+        cell.hidden = this.aos.find(ao => ao.name === cell.ao)?.hidden ?? false;
+        cell.hideText = this.filterText.length > 0 && !cell.qs?.some(q => {
+          return q.toLowerCase().includes(this.filterText.toLowerCase());
+        });
+      }
+    }
+
+    this.everythingHidden = this.aos.every(ao => ao.hidden);
+  }
+
+  private getCell(date: string, ao: string, aoMap = new Map<string, QCell>()):
+      QCell {
+    return aoMap.get(ao) ?? {
+      warning: false,
+      taken: false,
+      hidden: false,
+      hideText: false,
+      ao,
+      date,
+      closed: false,
+      text: null,
+      qs: [],
+      transparent: true,
+    };
+  }
+
+  private shouldBeHidden(set: Set<string>, text: string) {
+    // if there's no filter text, always show every item
+    if (text.length === 0) return false;
+
+    // hide empty rows/cols when there is a filter text
+    if (text.length > 0 && set.size === 0) return true;
+
+    return ![...set.values()].some(name => {
+      return name.toLowerCase().includes(text.toLowerCase());
+    });
   }
 }
