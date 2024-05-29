e058cf40cefc7696deb2446dbd3691efcad5c38f
Matt Taylor <taylor.matt777@gmail.com>
2024-05-29 13:54:40 -0600

    lint

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,12 @@
 import {CommonModule, CurrencyPipe} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 import {IonicModule} from '@ionic/angular';
 
 import {ActionsPopoverPageComponent} from './actions-popover/actions-popover-page.component';
 import {ActionsPopoverComponent} from './actions-popover/actions-popover.component';
+import {BackblastCardComponent} from './backblast-card/backblast-card.component';
 import {HeaderComponent} from './header/header.component';
 import {PaxAvatarComponent} from './pax-avatar/pax-avatar.component';
 import {PaxChipComponent} from './pax-chip/pax-chip.component';
@@ -12,8 +14,6 @@ import {PaxListItemComponent} from './pax-list-item/pax-list-item.component';
 import {PAXNameComponent} from './pax-name/pax-name.component';
 import {TimeFilterComponent} from './time-filter/time-filter.component';
 import {YearGridComponent} from './year-grid/year-grid.component';
-import { BackblastCardComponent } from './backblast-card/backblast-card.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
