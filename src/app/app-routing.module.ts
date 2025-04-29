c644b75e3b2e691847844a37cea277e4531020ea
Matt Taylor <taylor.matt777@gmail.com>
2025-04-29 12:08:19 -0600

    Add region route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     loadChildren: () => import('./pages/ao/ao.module').then(m => m.AoPageModule)
   },
   {
+    path: 'region/:region',
+    loadChildren: () => import('./pages/ao/ao.module').then(m => m.AoPageModule)
+  },
+  {
     path: 'dd/:name',
     loadChildren: () => import('./pages/ao/ao.module').then(m => m.AoPageModule)
   },
